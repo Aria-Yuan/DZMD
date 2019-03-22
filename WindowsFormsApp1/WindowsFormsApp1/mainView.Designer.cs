79e39b7a621be7acbe7fefe0763517ee2d71ca20
circle5566 <circle.z.yuan@gmail.com>
2019-03-22 23:46:46 +0800

    主界面动态心律图

---
+++ WindowsFormsApp1/WindowsFormsApp1/mainView.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class mainView
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.t = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button_Stop = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.patientId = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // t
+            // 
+            this.t.Interval = 1000;
+            this.t.Tick += new System.EventHandler(this.t_Tick);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 350F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.chart, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.36842F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.63158F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1006, 613);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // chart
+            // 
+            this.chart.BackColor = System.Drawing.SystemColors.Control;
+            this.chart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(356, 293);
+            this.chart.Margin = new System.Windows.Forms.Padding(6, 3, 3, 2);
+            this.chart.Name = "chart";
+            this.chart.Size = new System.Drawing.Size(647, 318);
+            this.chart.TabIndex = 8;
+            this.chart.Text = "chart1";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(180, 105);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 58);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "時間記錄";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(180, 20);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 58);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "時間記錄";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(26, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 58);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "藥物記錄";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(26, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 58);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "時間記錄";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button_Stop
+            // 
+            this.button_Stop.Location = new System.Drawing.Point(180, 212);
+            this.button_Stop.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Stop.Name = "button_Stop";
+            this.button_Stop.Size = new System.Drawing.Size(100, 32);
+            this.button_Stop.TabIndex = 9;
+            this.button_Stop.Text = "Stop";
+            this.button_Stop.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button_Stop);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 293);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(344, 317);
+            this.panel2.TabIndex = 15;
+            // 
+            // patientId
+            // 
+            this.patientId.AutoSize = true;
+            this.patientId.Location = new System.Drawing.Point(7, 20);
+            this.patientId.Name = "patientId";
+            this.patientId.Size = new System.Drawing.Size(50, 17);
+            this.patientId.TabIndex = 0;
+            this.patientId.Text = "病歷號";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "姓名";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "生日";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "科別";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(192, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "床號";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.patientId);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(344, 120);
+            this.panel1.TabIndex = 11;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(322, 98);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(15, 15);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(234, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(84, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(234, 17);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(84, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(63, 79);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(175, 22);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(63, 47);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(109, 22);
+            this.textBox6.TabIndex = 11;
+            // 
+            // mainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 613);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(1024, 660);
+            this.Name = "mainView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.mainView_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer t;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label patientId;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button_Stop;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/mainView.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting; 
+
+namespace WindowsFormsApp1
+{
+    public partial class mainView : Form
+    {
+        public mainView()
+        {
+            InitializeComponent();
+        }
+
+        Series series1, series2;
+        int sum = 1;
+        bool flag = false;
+
+        private void mainView_Load(object sender, EventArgs e)
+        {
+            CreateChart();
+            createSeries();
+            t.Start();
+        }
+
+        private void CreateChart()
+        {
+            ChartArea chartArea = new ChartArea();
+            chartArea.Name = "FirstArea";
+
+            chartArea.CursorX.IsUserEnabled = true;
+            chartArea.CursorX.IsUserSelectionEnabled = true;
+            chartArea.CursorX.SelectionColor = Color.SkyBlue;
+            chartArea.CursorY.IsUserEnabled = true;
+            chartArea.CursorY.AutoScroll = true;
+            chartArea.CursorY.IsUserSelectionEnabled = true;
+            chartArea.CursorY.SelectionColor = Color.SkyBlue;
+
+            chartArea.CursorX.IntervalType = DateTimeIntervalType.Auto;
+            chartArea.AxisX.ScaleView.Zoomable = false;
+            chartArea.AxisX.ScrollBar.ButtonStyle = ScrollBarButtonStyles.All;//启用X轴滚动条按钮
+
+            chartArea.BackColor = Color.White;                      //背景色
+            //chartArea.BackSecondaryColor = Color.White;                 //渐变背景色
+            //chartArea.BackGradientStyle = GradientStyle.TopBottom;      //渐变方式
+            chartArea.BackHatchStyle = ChartHatchStyle.None;            //背景阴影
+            chartArea.BorderDashStyle = ChartDashStyle.NotSet;          //边框线样式
+            chartArea.BorderWidth = 1;                                  //边框宽度
+            chartArea.BorderColor = Color.DarkGray;
+
+            chartArea.AxisX.MajorGrid.Enabled = true;
+            chartArea.AxisX.MajorGrid.LineColor = Color.DarkGray;
+            chartArea.AxisY.MajorGrid.Enabled = true;
+            chartArea.AxisY.MajorGrid.LineColor = Color.DarkGray;
+
+            // Axis
+            chartArea.AxisY.LabelAutoFitStyle = LabelAutoFitStyles.None;
+            chartArea.AxisY.LabelStyle.Font = new Font(chartArea.AxisY.LabelStyle.Font.Name, 9);
+            chartArea.AxisY.Maximum = 240;
+            chartArea.AxisY.Minimum = 0;
+            chartArea.AxisY.Interval = 20;
+            chartArea.AxisY.LineWidth = 1;
+            chartArea.AxisY.LineColor = Color.Black;
+            chartArea.AxisY.Enabled = AxisEnabled.True;
+            
+            chartArea.AxisX.IsLabelAutoFit = true;
+            chartArea.AxisX.LabelAutoFitMinFontSize = 5;
+            chartArea.AxisX.LabelStyle.Angle = -15;
+
+
+            chartArea.AxisX.LabelStyle.IsEndLabelVisible = true;        //show the last label
+            chartArea.AxisX.Interval = 10;
+            chartArea.AxisX.IntervalAutoMode = IntervalAutoMode.FixedCount;
+            chartArea.AxisX.IntervalType = DateTimeIntervalType.NotSet;
+            chartArea.AxisX.TextOrientation = TextOrientation.Auto;
+            chartArea.AxisX.LineWidth = 1;
+            chartArea.AxisX.LineColor = Color.Black;
+            chartArea.AxisX.Enabled = AxisEnabled.True;
+            chartArea.AxisX.ScaleView.MinSizeType = DateTimeIntervalType.Months;
+            chartArea.AxisX.Crossing = 0;
+            chartArea.AxisX.LabelStyle.Enabled = false;
+
+            chartArea.Position.Height = 95;
+            chartArea.Position.Width = 98;
+            chartArea.Position.X = 1;
+            chartArea.Position.Y = 1;
+
+            chart.ChartAreas.Add(chartArea);
+            chart.BackGradientStyle = GradientStyle.TopBottom;
+            //图表的边框颜色、
+            chart.BorderlineColor = Color.DarkGray;
+            //图表的边框线条样式
+            chart.BorderlineDashStyle = ChartDashStyle.DashDot;
+            //图表边框线条的宽度
+            chart.BorderlineWidth = 1;
+            chart.BackColor = SystemColors.Control;
+            chart.BackSecondaryColor = SystemColors.Control;
+            //图表边框的皮肤
+            //chart.BorderSkin.SkinStyle = BorderSkinStyle.None;
+
+        }
+
+        private void createSeries()
+        {
+            //收缩压
+            series1 = new Series();
+            series1.ChartArea = "FirstArea";
+            chart.Series.Add(series1);
+
+            //Series1 style
+            series1.ToolTip = "#VALX,#VALY";    //鼠标停留在数据点上，显示XY值
+
+            series1.Name = "series1";
+            series1.ChartType = SeriesChartType.Spline;  // type
+            series1.BorderWidth = 0;
+            series1.Color = Color.Red;
+            series1.XValueType = ChartValueType.Time;//x axis type
+            series1.YValueType = ChartValueType.Int32;//y axis type
+
+            //Marker
+            series1.MarkerStyle = MarkerStyle.Triangle;
+            series1.MarkerSize = 8;
+            series1.MarkerColor = Color.Red;
+
+            //舒张压
+            series2 = new Series();
+            series2.ChartArea = "FirstArea";
+            chart.Series.Add(series2);
+
+            //Series1 style
+            series2.ToolTip = "#VALX,#VALY";    //鼠标停留在数据点上，显示XY值
+
+            series2.Name = "series2";
+            series2.ChartType = SeriesChartType.Spline;  // type
+            series2.BorderWidth = 0;
+            series2.Color = Color.Red;
+            series2.XValueType = ChartValueType.Time;//x axis type
+            series2.YValueType = ChartValueType.Int32;//y axis type
+
+            //Marker
+            series2.MarkerStyle = MarkerStyle.Triangle;
+            series2.MarkerSize = 8;
+            series2.MarkerColor = Color.Blue;
+
+            this.chart.Legends.Clear();
+        }
+
+
+        static int range = 0, range1 = 0;
+        Random r = new Random(6);
+        Random r1 = new Random(9);
+
+        //用来设置切换视图时的视角
+        private void chart_SelectionRangeChanged(object sender, CursorEventArgs e)
+        {
+            //无数据时返回
+            if (chart.Series[0].Points.Count == 0)
+                return;
+
+            double start_position = 0.0;
+            double end_position = 0.0;
+            double myInterval = 0.0;
+            start_position = e.NewSelectionStart;
+            end_position = e.NewSelectionEnd;
+            myInterval = Math.Abs(start_position - end_position);
+            if (myInterval == 0.0)
+                return;
+
+            //X轴视图起点
+            chart.ChartAreas[0].AxisX.ScaleView.Position = Math.Min(start_position, end_position);
+            //X轴视图长度
+            chart.ChartAreas[0].AxisX.ScaleView.Size = myInterval;
+            //X轴间隔
+            if (myInterval < 11.0)
+            {
+                chart.ChartAreas[0].AxisX.Interval = 1;
+            }
+            else
+            {
+                chart.ChartAreas[0].AxisX.Interval = Math.Floor(myInterval / 10);
+            }
+            flag = true;
+
+        }
+
+        private void t_Tick(object sender, EventArgs e)         //timer事件
+        {
+            if (flag)
+                return;
+            else
+            {
+                range = r.Next(140, 200);    //随机取数 
+                range1 = r1.Next(80, 120);
+                series1.Points.AddXY(sum, range);   //设置series点    
+                series2.Points.AddXY(sum, range1);
+                sum++;
+                chart.ChartAreas[0].AxisX.Interval = 1D;
+                chart.ChartAreas[0].AxisX.ScaleView.Size = 10D;
+                if (sum <= chart.ChartAreas[0].AxisX.ScaleView.Size)
+                    chart.ChartAreas[0].AxisX.ScaleView.Position = 1;
+                else
+                    chart.ChartAreas[0].AxisX.ScaleView.Position = sum - chart.ChartAreas[0].AxisX.ScaleView.Size;
+            }
+        }
+
+        private void button_Stop_Click(object sender, EventArgs e)  //切换停止开始按钮
+        {
+            switch (button_Stop.Text)
+            {
+                case "Stop":
+                    {
+                        button_Stop.Text = "Start";
+                        t.Stop();
+                        break;
+                    }
+                case "Start":
+                    {
+                        button_Stop.Text = "Stop";
+                        t.Start();
+                        break;
+                    }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+    
+}
