f8f7150e65b550f9afbb2567057e284c3b05bbe5
circle5566 <circle.z.yuan@gmail.com>
2019-04-21 23:45:02 +0800

    药物资料随timer更新，药物list

--- WindowsFormsApp1/WindowsFormsApp1/mainView.cs
+++ WindowsFormsApp1/WindowsFormsApp1/mainView.cs
@@ -13,26 +13,32 @@ namespace WindowsFormsApp1
 {
     public partial class MainView : Form
     {
+
+        private double x, l;
         public MainView()
         {
             InitializeComponent();
         }
 
-        Series series1, series2, series3;
-        int sum = 1;
-        bool flag = false;
-        bool stop_flag = false;
+        private Series series1, series2, series3;
+        private int sum = 1;
+        private bool flag = false;
+        private bool stop_flag = false;
 
         private void mainView_Load(object sender, EventArgs e)
         {
+            this.MaximumSize = new Size(this.Width, this.Height);
+            x = this.Size.Width - this.MinimumSize.Width;
+            l = this.MaximumSize.Width - this.MinimumSize.Width;
             CreateChart();
-            CreateMedicineChart();
+            //CreateMedicineChart();
             createSeries();
-            medicineLst_init();
+            createViewList();
+            //medicineLst_init();
             t.Start();
-            this.MaximumSize = new Size(this.Width,this.Height);
         }
 
+        //生命体征讯号
         private void CreateChart()
         {
             ChartArea chartArea = new ChartArea();
@@ -59,8 +65,8 @@ namespace WindowsFormsApp1
             chartArea.AxisY.Interval = 10;
             chartArea.AxisY.LineWidth = 1;
             chartArea.AxisY.LineColor = Color.Black;
-            chartArea.AxisY.Enabled = AxisEnabled.True;
-            
+            chartArea.AxisY.Enabled = AxisEnabled.Auto;
+
             chartArea.AxisX.IsLabelAutoFit = true;
             chartArea.AxisX.LabelAutoFitMinFontSize = 5;
             chartArea.AxisX.LabelStyle.Angle = -15;
@@ -79,10 +85,10 @@ namespace WindowsFormsApp1
             chartArea.AxisX.Crossing = 0;
             chartArea.AxisX.LabelStyle.Enabled = false;
             
-            chartArea.Position = new ElementPosition(7, 2, 92, 97);
+            chartArea.Position = new ElementPosition((float)5.9, 2, (float)93.04, 97);
 
             chart.ChartAreas.Add(chartArea);
-            chart.BackGradientStyle = GradientStyle.TopBottom;
+            chart.BackGradientStyle = GradientStyle.None;
             //图表的边框颜色
             chart.BorderlineColor = Color.DarkGray;
             //图表的边框线条样式
@@ -90,96 +96,48 @@ namespace WindowsFormsApp1
             //图表边框线条的宽度
             chart.BorderlineWidth = 1;
             chart.BackColor = SystemColors.Control;
-            chart.BackSecondaryColor = SystemColors.Control;
             //图表边框的皮肤
             //chart.BorderSkin.SkinStyle = BorderSkinStyle.None;
 
         }
 
-        private void CreateMedicineChart()
+        //已加入的药物list
+        static List<Medicinedata> medicineList;
+
+        private void createViewList()
         {
-            ChartArea chartArea = new ChartArea();
-            chartArea.Name = "MedicineArea";
-
-            chartArea.AxisX.ScrollBar.ButtonStyle = ScrollBarButtonStyles.All;//启用X轴滚动条按钮
-
-            chartArea.BackColor = Color.Snow;                      //背景色
-            chartArea.BackHatchStyle = ChartHatchStyle.None;            //背景阴影
-            chartArea.BorderDashStyle = ChartDashStyle.NotSet;          //边框线样式
-            chartArea.BorderWidth = 1;                                  //边框宽度
-            chartArea.BorderColor = Color.DarkGray;
-
-            chartArea.AxisX.MajorGrid.Enabled = true;
-            chartArea.AxisX.MajorGrid.LineColor = Color.DarkGray;
-            chartArea.AxisY.MajorGrid.Enabled = true;
-            chartArea.AxisY.MajorGrid.LineColor = Color.DarkGray;
-
-            // Axis
-            chartArea.AxisY.LabelAutoFitStyle = LabelAutoFitStyles.None;
-            chartArea.AxisY.LabelStyle.Font = new Font(chartArea.AxisY.LabelStyle.Font.Name, 9);
-            chartArea.AxisY.Maximum = 360;
-            chartArea.AxisY.Minimum = 0;
-            chartArea.AxisY.Interval = 20;
-            chartArea.AxisY.LineWidth = 1;
-            chartArea.AxisY.LineColor = Color.Black;
-            chartArea.AxisY.Enabled = AxisEnabled.True;
-            chartArea.AxisY.IsLabelAutoFit = false;
-
-            chartArea.AxisX.IsLabelAutoFit = true;
-            chartArea.AxisX.LabelAutoFitMinFontSize = 5;
-            chartArea.AxisX.LabelStyle.Angle = -15;
-
-            chartArea.AxisX.Interval = 5D;
-            chartArea.AxisX.ScaleView.Size = chart.ChartAreas[0].AxisX.ScaleView.Size;
-
-            chartArea.AxisX.LabelStyle.IsEndLabelVisible = true;        //show the last label
-            chartArea.AxisX.IntervalAutoMode = IntervalAutoMode.FixedCount;
-            chartArea.AxisX.IntervalType = DateTimeIntervalType.NotSet;
-            chartArea.AxisX.TextOrientation = TextOrientation.Auto;
-            chartArea.AxisX.LineWidth = 1;
-            chartArea.AxisX.LineColor = Color.Black;
-            chartArea.AxisX.Enabled = AxisEnabled.True;
-            chartArea.AxisX.ScaleView.MinSizeType = DateTimeIntervalType.Months;
-            chartArea.AxisX.Crossing = 0;
-            chartArea.AxisX.LabelStyle.Enabled = false;
-            
-            chartArea.Position = new ElementPosition(0, 0, 99, 97);
-
-            medicineChart.ChartAreas.Add(chartArea);
-            medicineChart.BackGradientStyle = GradientStyle.TopBottom;
-            //图表的边框颜色、
-            medicineChart.BorderlineColor = Color.DarkGray;
-            //图表的边框线条样式
-            medicineChart.BorderlineDashStyle = ChartDashStyle.NotSet;
-            //图表边框线条的宽度
-            medicineChart.BorderlineWidth = 1;
-            medicineChart.BackColor = SystemColors.Control;
-            medicineChart.BackSecondaryColor = SystemColors.Control;
-            //图表边框的皮肤
-            //chart.BorderSkin.SkinStyle = BorderSkinStyle.None;
-
-        }
-
-        private void medicineLst_init()
-        {
+            //medicineList.BackColor = Color.Snow;
             // 设置行高
             ImageList imgList = new ImageList();
             // 分别是宽和高
             imgList.ImageSize = new Size(1, 25);
             // 这里设置listView的SmallImageList ,用imgList将其撑大
+            medicineData.SmallImageList = imgList;
             medicineLst.SmallImageList = imgList;
 
-            this.medicineLst.Columns.Add("");
-            this.medicineLst.Columns[0].Width = -1;
-            this.medicineLst.View = View.Details;
+            this.medicineLst.Columns.Add("", 0, HorizontalAlignment.Center);
+            this.medicineLst.Columns.Add("",this.medicineLst.Width,HorizontalAlignment.Center);
+            this.medicineLst.HeaderStyle = ColumnHeaderStyle.None;
+            //随机产生，测试资料
             for (int i = 0; i < 10; i++)
             {
                 ListViewItem it = new ListViewItem();
-                it.Text = "medicine" + i;
+                it.Text = "藥物" + i;
+                it.SubItems.Add("藥物" + i);
                 medicineLst.Items.Add(it);
+                it = new ListViewItem();
+                it.Text = "";
+                medicineData.Items.Add(it);
             }
+            this.medicineLst.View = View.Details;
+
+            String date_time = DateTime.Now.ToShortTimeString() + "";
+            String[] date_time2 = date_time.Split(' ');
+            this.medicineData.Columns.Add(date_time2[1],0);
+            this.medicineData.HeaderStyle = ColumnHeaderStyle.None;
+            this.medicineData.View = View.Details;
         }
-        
+
         private void createSeries()
         {
             //收缩压
@@ -252,6 +210,24 @@ namespace WindowsFormsApp1
             this.chart.Legends.Clear();
         }
 
+        //添加剂量
+        private void drawMedicineData()
+        {
+            foreach (ColumnHeader ch in medicineData.Columns)
+            {
+                ch.Width = this.medicineData.Width / (int)(((x / l * 20) + 30 * (
+                                                                1 + trackBar1.Value / 20.0)) / 5);
+            }
+
+            for (int i = 0; i < 1; i++)
+            {
+                ListViewItem lv = medicineData.Items[0];
+                lv.SubItems.Add("剂量" + i);
+                medicineData.Items.RemoveAt(i);
+                medicineData.Items.Insert(i,lv);
+            }
+            
+        }
 
         static int range = 0, range1 = 0, range2 = 0;
         Random r = new Random(6);
@@ -271,27 +247,39 @@ namespace WindowsFormsApp1
             series1.Points.AddXY(sum, range);   //设置series点    
             series2.Points.AddXY(sum, range1);
             series3.Points.AddXY(sum, range2);
+            //判断timer动了几次
+            if (sum%5 == 1)
+            {
+                this.medicineData.Columns.Add("");
+                drawMedicineData();
+                this.medicineData.View = View.Details;
+            }
             sum++;
-            if (sum < chart.ChartAreas[0].AxisX.ScaleView.Size)
+            if (sum <= chart.ChartAreas[0].AxisX.ScaleView.Size)
                 chart.ChartAreas[0].AxisX.ScaleView.Position = 0;
-            else if(!stop_flag)
+            //记得优化多一格的问题
+            else if (!stop_flag)
+            {
+                chart.ChartAreas[0].AxisX.ScaleView.Size = (int)(((x / l * 20) + 30 * (
+                                                                1 + trackBar1.Value / 20.0)) / 5) * 5 - 1;
                 chart.ChartAreas[0].AxisX.ScaleView.Position = sum - chart.ChartAreas[0].AxisX.ScaleView.Size;
+            }
+
         }
 
         
         //随窗口大小变动事件
         private void mainView_SizeChanged(object sender, EventArgs e)
         {
+            x = this.Size.Width - this.MinimumSize.Width;
+            l = this.MaximumSize.Width - this.MinimumSize.Width;
             if (chart.ChartAreas.Count != 0)
             {
                 chart.ChartAreas[0].AxisX.Interval = 1D;
-                medicineChart.ChartAreas[0].AxisX.Interval = 5D;
-                double x = this.Size.Width - this.MinimumSize.Width;
-                double l = this.MaximumSize.Width - this.MinimumSize.Width;
-                chart.ChartAreas[0].AxisX.ScaleView.Size = (int)((x / l * 20) + 30 * (
-                                                            1 + trackBar1.Value / 20.0));
-                medicineChart.ChartAreas[0].AxisX.ScaleView.Size = (int)((x / l * 20) + 30 * (
-                                                            1 + trackBar1.Value / 20.0));
+                //medicineChart.ChartAreas[0].AxisX.Interval = 5D;
+                chart.ChartAreas[0].AxisX.ScaleView.Size = (int)(((x / l * 20) + 30 * (
+                                                            1 + trackBar1.Value / 20.0))/5)*5;
+                //medicineChart.ChartAreas[0].AxisX.ScaleView.Size = (int)((x / l * 20) + 30 * (1 + trackBar1.Value / 20.0));
 
                 if (this.Size.Height == this.MaximumSize.Height)
                 {
@@ -309,18 +297,9 @@ namespace WindowsFormsApp1
         //调整生命体征图形大小
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
-            double x = this.Size.Width - this.MinimumSize.Width;
-            double l = this.MaximumSize.Width - this.MinimumSize.Width;
-            chart.ChartAreas[0].AxisX.ScaleView.Size = (int)(((x / l * 20) + 30) * (
-                                                         1 + trackBar1.Value / 20.0));
-        }
-
-        private void medicineLst_ColumnWidthChanging(object sender, ColumnWidthChangingEventArgs e)
-        {
-            // 取消掉正在调整的事件
-            e.Cancel = true;
-            // 把新宽度恢复到之前的宽度
-            e.NewWidth = this.medicineLst.Columns[e.ColumnIndex].Width;
+            chart.ChartAreas[0].AxisX.ScaleView.Size = (int)(((x / l * 20) + 30 * (
+                                                            1 + trackBar1.Value / 20.0))/5) * 5;
+            drawMedicineData();
         }
 
         private void button_Stop_Click(object sender, EventArgs e)  //切换停止开始按钮
