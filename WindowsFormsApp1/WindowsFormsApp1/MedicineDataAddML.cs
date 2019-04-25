0077c47fa128d8de30d12584f03b1f0953648a19
event1852729 <event1852729@gmail.com>
2019-04-25 22:04:11 +0800

    藥物新增視窗確認後添加mark
    還有點左邊藥物列表可以再繼續打藥

---
+++ WindowsFormsApp1/WindowsFormsApp1/MedicineDataAddML.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MedicineDataAddML : Form
+    {
+        MainView mainView;
+        public MedicineDataAddML()
+        {
+            InitializeComponent();
+        }
+        public MedicineDataAddML(MainView mainView)
+        {
+            InitializeComponent();
+            this.mainView = mainView;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            mainView.update_Medicine_mark(textBox1.Text);
+         
+
+        }
+    }
+}
